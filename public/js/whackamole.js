ff332f84516c6c3f31dc343c4005f5f0b8f834b3
Fernando Renteria <rentfern@gmail.com>
2015-06-04 20:01:37 -0500

    Game is fully functional: Clicking on mole adds point, clicking on empty hole subtracts points, having no lives left alerts user that it's game over

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -4,32 +4,62 @@
 	var score;
 	var holes;
 	var random;
+	var lives;
 	var intervalId;
 
 	//initializes variables, button, and beginning animations whenpage is loaded
 	function initialize(){
 		round = 1;
 		score = 0;
+		lives = 5;
 		holes = $(".hole");
-		setBeginningAnimation();
+		setHoleAnimation();
 
 		$("#start-button").on("click", onButtonClick);
 	}
 
-	function setBeginningAnimation(){
+	//before game begins, highlight random holes in gameboard
+	function setHoleAnimation(){
 		var interval = 1000;
+		var mole;
 
 		intervalId = setInterval(function(){
+			//before getting a new random hole, make sure the other one doesn't have a mole anymore
+			$(holes[random]).toggleClass("mole");
+
 			random = Math.floor(Math.random() * holes.length);
 
 			$(holes[random]).effect("highlight", {color:"#558ABB"});
+			$(holes[random]).toggleClass("mole");
 		}, interval);
 	}
 
+	//when start button is clicked, remove button, stop highlighting, and make gameboard opaque
 	function onButtonClick(){
 		clearInterval(intervalId);
 		$("#start-button").remove();
 		$("#gameboard").css("opacity", "1.0");
+
+		startGame();
+	}
+
+	function onHoleClick(event){
+		if($(this).hasClass("mole")){
+			score++;
+			$("#score").text(score);
+		} else{
+			lives--;
+			$("#lives").text(lives);
+		}
+
+		if (lives < 0){
+			alert("Game Over");
+		}
+	}
+
+	function startGame(){
+		setHoleAnimation();
+		$(holes).on("click", onHoleClick);
 	}
 
 	initialize();
