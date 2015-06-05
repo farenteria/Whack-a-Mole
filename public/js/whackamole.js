45264d4b82b66d96e1a933322049a0422ffeddc6
Fernando Renteria <rentfern@gmail.com>
2015-06-04 19:43:32 -0500

    Moved button so it wouldn't be under gameboard. Clicking start button now makes gameboard opaque, and stops holes form highlighting to begin the game

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -4,23 +4,32 @@
 	var score;
 	var holes;
 	var random;
+	var intervalId;
 
+	//initializes variables, button, and beginning animations whenpage is loaded
 	function initialize(){
 		round = 1;
 		score = 0;
 		holes = $(".hole");
 		setBeginningAnimation();
+
+		$("#start-button").on("click", onButtonClick);
 	}
 
 	function setBeginningAnimation(){
 		var interval = 1000;
-		var intervalId;
 
 		intervalId = setInterval(function(){
 			random = Math.floor(Math.random() * holes.length);
 
 			$(holes[random]).effect("highlight", {color:"#558ABB"});
 		}, interval);
+	}
+
+	function onButtonClick(){
+		clearInterval(intervalId);
+		$("#start-button").remove();
+		$("#gameboard").css("opacity", "1.0");
 	}
 
 	initialize();
